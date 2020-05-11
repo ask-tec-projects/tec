cc312fcae5f46761b0a0d5e6a5236d8ae6b2773d
Andreas Kruhlmann <kruhlmann@protonmail.com>
2020-05-11 15:25:30 +0200

    Fixed add users

--- ad/task_2_and_3/create_shares.ps1
+++ ad/task_2_and_3/create_shares.ps1
@@ -9,4 +9,8 @@ new-smbshare -path "C:\share\resources" -fullaccess "TEC\Administrator" -readacc
 
 for ($i=0; $i -lt $grps.length; $i++) {
     mkdir $grps[$i]
+    $acl = "get-acl \\server1\resources\$grps[$i]"
+    $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("ENTERPRISE\T.Simpson","FullControl","Allow")
+
+$acl.SetAccessRule($AccessRule)
 }
